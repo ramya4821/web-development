a777110eb7d77e5b9f49810bb43d9de5184aaf90
ramya4821 <akularamya482@gmail.com>
2025-03-31 09:52:44 +0530

    htmlcssjs

---
+++ code/javasrcipt/script.js
@@ -0,0 +1,82 @@
+/*let a=5; conditional statements//
+let b=3;
+if(a>b)
+{
+    console.log(`${a} is greaterthan ${b}`)
+}else if(b>a)
+{
+    console.log(`${b} is greaterthan ${a}`)
+
+}
+else{
+    console.log(`${b} is equal to ${a}`)
+
+}
+    */
+
+  /* let value=6; switch cases 
+   switch(value)
+   {
+    case 1 :console.log("monday"); break;
+    case 2: console.log("tues");break;
+    case 3 :console.log("wesd");break;
+    case 4 :console.log("turs");break;
+    case 5 :console.log("friday") ;break;
+    case 6 :console.log("saturday");break;
+     
+    default :console.log("sunday")
+
+
+
+   }*/
+  /*
+  let i=0;
+  while (i=<10);
+  {
+    console.log(i);
+    i++;
+  }
+*/
+/*let i = "ramya"
+n=i.length-1;
+do
+{
+    console.log(i.charAt(n))
+     n--
+}
+while(n<=0)*/
+/*
+for(i=20;i>=2;i-=2)
+{
+    console.log(i)
+}*/
+/*let arr=[1,2,3,4,5]
+
+//
+ /*for(let i=0;i<=arr.lenth;i++)
+{
+    console.log(arr[i])
+}*/
+let c= document.getElementById("in")
+let v=document.getElementById("i")
+let add=document.getElementById()
+c=0
+v=0
+function fun ()
+{
+if(c>0  v>0)
+{
+
+}
+    
+
+
+    
+
+}
+
+
+
+
+
+
